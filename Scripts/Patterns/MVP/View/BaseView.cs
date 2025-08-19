8d2c62ef91e9818bef335f2932a325aafb7c87f6
Dung <dungnguyen260103@gmail.com>
2025-08-20 02:23:48 +0700

    add CanvasGroup property to BaseView

--- Scripts/Patterns/MVP/View/BaseView.cs
+++ Scripts/Patterns/MVP/View/BaseView.cs
@@ -2,9 +2,23 @@ namespace GameFoundation.Scripts.Patterns.MVP.View
 {
     using UnityEngine;
 
+    [RequireComponent(typeof(CanvasGroup))]
     public abstract class BaseView : MonoBehaviour, IView
     {
-        protected bool isInitialized;
+        protected bool        isInitialized;
+        protected CanvasGroup canvasGroup;
+
+        public CanvasGroup CanvasGroup
+        {
+            get
+            {
+                if (!this.canvasGroup)
+                {
+                    this.canvasGroup = this.GetComponent<CanvasGroup>();
+                }
+                return this.canvasGroup;
+            }
+        }
 
         public virtual void Initialize()
         {
