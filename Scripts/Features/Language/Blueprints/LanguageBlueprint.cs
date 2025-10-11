673c1f39961889fc88219e939f526d12fb1b4856
Dung <dungnguyen260103@gmail.com>
2025-10-11 11:36:42 +0700

    feat: implement localization support with language files and blueprint

--- Scripts/Features/Language/Blueprints/LanguageBlueprint.cs
+++ Scripts/Features/Language/Blueprints/LanguageBlueprint.cs
@@ -1,27 +1,20 @@
 namespace GameFoundation.Scripts.Features.Language.Blueprints
 {
-    using System.Collections;
+    using System;
     using System.Collections.Generic;
-    using GameFoundation.Scripts.Features.Language.Editor;
-    using Sirenix.OdinInspector;
-    using UnityEngine;
 
-    [CreateAssetMenu(fileName = "LanguageBlueprint", menuName = "HyperCasual/Language/LanguageBlueprint")]
-    public class LanguageBlueprint : ScriptableObject
+    [Serializable]
+    public class LanguageBlueprint
     {
-        #if UNITY_EDITOR
-        [ValueDropdown(nameof(ShowListLanguage), IsUniqueList = true)]
-        #endif
         public string initLanguage;
+        public string defaultLanguage;
+        public List<LanguageFileReference> languages = new();
+    }
 
-        public LanguageData       defaultLanguageData;
-        public List<LanguageData> LanguageDatas;
-
-        #if UNITY_EDITOR
-        private IEnumerable ShowListLanguage()
-        {
-            return LanguageGlobalConfig.Instance.Languages;
-        }
-        #endif
+    [Serializable]
+    public class LanguageFileReference
+    {
+        public string languageName;
+        public string address;
     }
 }
